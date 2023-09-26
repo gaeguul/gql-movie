58b511036d4178e0971b5c81993cddf2f19ae429
gaeguul <jeeham0226@gmail.com>
2023-09-26 15:08:05 +0900

    Get Movies API by using useQuery

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,5 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ALL_MOVIES = gql`
+  {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
+
 const Movies = () => {
-  return <div>List of Movie</div>;
+  const { data, loading, error } = useQuery(GET_ALL_MOVIES);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Could not Fetch !</div>;
+  return (
+    <div>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
