4bd131af70456b969a22c579d9c4371b7e73985f
gaeguul <jeeham0226@gmail.com>
2023-09-28 00:11:54 +0900

    CSS of pages

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,29 +1,78 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { RxStarFilled } from "react-icons/rx";
 
 const GET_MOVIE = gql`
   query getMovie($movieId: String!) {
     movie(id: $movieId) {
       id
       title
+      rating
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+const Info = styled.div`
+  margin-left: 10px;
+  width: 40%;
+`;
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: 600;
+  margin-bottom: 15px;
+`;
+const Subtitle = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  display: flex;
+`;
+const StarIcon = styled(RxStarFilled)`
+  color: #ffe11b;
+`;
+const Rating = styled.div`
+  margin-left: 7px;
+`;
+const Image = styled.div`
+  width: 40%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 4px 12px 20px 6px rgba(0, 0, 0, 0.3);
+`;
+
 const Movie = () => {
   const { id } = useParams();
-  const { data, loading, error } = useQuery(GET_MOVIE, {
+  const { data, loading } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Could not Fetch !</div>;
   return (
-    <div>
-      <h1>{data.movie.title}</h1>
-      <h2>{data.movie.id}</h2>
-    </div>
+    <Container>
+      <Info>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>
+          <StarIcon />
+          <Rating>{data?.movie?.rating}</Rating>
+        </Subtitle>
+      </Info>
+      <Image image={data?.movie?.medium_cover_image} />
+    </Container>
   );
 };
 
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,27 +1,91 @@
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_ALL_MOVIES = gql`
   {
     allMovies {
       id
       title
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.div`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 200px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: 600;
+`;
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 25px;
+  width: 90%;
+  position: relative;
+  top: -20px;
+`;
+const PosterContainer = styled.div`
+  height: 400px;
+  border-radius: 20px;
+  width: 100%;
+  background-color: transparent;
+  transition: transform 0.16s ease-out;
+  box-shadow: 4px 12px 20px 6px rgba(0, 0, 0, 0.2);
+  &:hover {
+    transform: scale(1.007);
+  }
+`;
+const PosterImage = styled.div`
+  background-image: url(${(props) => props.image});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 30px;
+`;
+const Error = styled.div``;
+
 const Movies = () => {
   const { data, loading, error } = useQuery(GET_ALL_MOVIES);
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Could not Fetch !</div>;
   return (
-    <div>
-      {data.allMovies.map((movie) => (
-        <li key={movie.id}>
-          <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-        </li>
-      ))}
-    </div>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      <MoviesGrid>
+        {data?.allMovies?.map((movie) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movie/${movie.id}`}>
+              <PosterImage image={movie.medium_cover_image} />
+            </Link>
+          </PosterContainer>
+        ))}
+      </MoviesGrid>
+      {loading && <Loading>Loading...</Loading>}
+      {error && <Error>Could not Fetch !</Error>}
+    </Container>
   );
 };
 
